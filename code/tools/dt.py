0918706a26c20e973b57db118f41a273c75158a5
wsj1988 <980536572@qq.com>
2017-06-30 17:20:41 +0800

    update directory structure

---
+++ code/tools/dt.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+def str_2_ts(str_dt):
+    """convert string datetime to timestamp"""
+    pass
+
+def ts_2_str(ts_dt):
+    """convert timestamp to string, notation as `%Y-%m-%d %H:%M:%S`"""
+    pass
---
+++ code/tools/ip.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def is_ip(str_ip):
+    """whether given dot-decimal IP is legal"""
+    pass
+
+def is_public_ip(str_ip):
+    """whether given dot-decimal IP is public"""
+    pass
+
+def is_private_ip(str_ip):
+    """whether given dot-decimal IP is private"""
+    pass
